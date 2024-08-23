ff9c2c995c8a7c4331a98f30ad5a6aea2a3f9166
Sebastian-Santiago <95436574+Sebastian-Santiago@users.noreply.github.com>
2024-08-23 16:29:52 +0000

    Ejercicio 3 del lab 01

---
+++ lab01.cpp
@@ -0,0 +1,13 @@
+
+// SEBASTIAN SANTIAGO OYOLA CCOM 3033
+// A program to practice modifications and compilation
+
+#include <iostream>
+
+int main() {
+  std::cout << "Hello World!" << std::endl;
+  std::cout << "This is a 1!" << std::endl;
+  
+  return 0;
+}
+
